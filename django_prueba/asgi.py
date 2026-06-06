8f03e11afffb8ac222d857cc3c9a676cc8a38252
GD <delgadoalvarez544@gmail.com>
2026-06-06 10:46:46 -0500

    Fix AppRegistryNotReady en asgi

--- django_prueba/asgi.py
+++ django_prueba/asgi.py
@@ -1,24 +1,20 @@
-"""
-ASGI config for django_prueba project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django_prueba.routing import websocket_urlpatterns
 
+# 1. Le decimos a Django dónde están sus configuraciones
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_prueba.settings')
 
-application = get_asgi_application()
- 
+# 2. ¡MUY IMPORTANTE! Encendemos el motor de Django ANTES de importar los WebSockets
+django_asgi_app = get_asgi_application()
+
+# 3. AHORA SÍ, importamos las herramientas de Channels y tus rutas
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django_prueba.routing import websocket_urlpatterns
+
+# 4. Declaramos la aplicación principal que usará Daphne
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
