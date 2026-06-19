19f858efdf4f2c5e0e05675e50f780f540ee0f75
GD <delgadoalvarez544@gmail.com>
2026-06-18 22:07:55 -0500

    calajo

--- django_prueba/asgi.py
+++ django_prueba/asgi.py
@@ -1,14 +1,15 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django_prueba.routing import websocket_urlpatterns
 
 # 1. Le decimos a Django dónde están sus configuraciones
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_prueba.settings')
 
 # 2. ¡MUY IMPORTANTE! Encendemos el motor de Django ANTES de importar los WebSockets
 django_asgi_app = get_asgi_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django_prueba.routing import websocket_urlpatterns
+
 
 # 3. Declaramos la aplicación principal que usará Daphne
 application = ProtocolTypeRouter({
